d43e62753f86fd0afe5be7aa2a3a8afabefa737d
Michal <michal.jablonski097@gmail.com>
2020-04-15 12:59:47 +0100

    feat:styles refactoring for projects section

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,5 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
 
 
 console.log(`Cześć, na tej stronie znajdziesz informacje o moich projektach, mam nadzieję że Ci się podoba`);
@@ -23,6 +8,8 @@ console.log(`hello, you can find here my projects, hope you like it :)`);
 const list = document.querySelector('.projects__list--js');
 const script = document.querySelector (".projects-list__footer--script")
 const demo = document.querySelector (".projects-list__footer--demo")
+
+
 
 fetch('https://api.github.com/users/waveex/repos?sort=updated&direction=desc')
   .then(resp => resp.json())
@@ -33,7 +20,7 @@ fetch('https://api.github.com/users/waveex/repos?sort=updated&direction=desc')
       list.innerHTML += `
       <li class="projects-list__item">
       <img class="projects-list__img" src="../assets/img/github.svg" alt="" />
-      <a class="projects-list__link" href="${html_url}">${name}</a>
+      <a class="projects-list__link" }">${name}</a>
       <p class="projects-list__link--description">${description ? description : "brak opisu"}</p>
       <div class=" projects-list__footer">
 
@@ -47,3 +34,7 @@ fetch('https://api.github.com/users/waveex/repos?sort=updated&direction=desc')
   .catch(err => {
     console.log(err);
   })
+/*demo.addEventListener('click' , (e) => {
+
+}
+)*/
