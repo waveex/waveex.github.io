ecf2b0603813cfd5caa5d88194b1b4c413419a29
Michal <michal.jablonski097@gmail.com>
2020-04-23 12:22:56 +0200

    Javascript update

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,9 @@
 "use strict";
 
 
-
 console.log(`Cześć, na tej stronie znajdziesz informacje o moich projektach, mam nadzieję że Ci się podoba`);
 console.log(`hello, you can find here my projects, hope you like it :)`);
 
-const list = document.querySelector('.projects__list--js');
-const script = document.querySelector (".projects-list__footer--script")
-const demo = document.querySelector (".projects-list__footer--demo")
 
 
 
@@ -15,26 +11,37 @@ fetch('https://api.github.com/users/waveex/repos?sort=updated&direction=desc')
   .then(resp => resp.json())
   .then(resp => {
     const repos = resp;
+
+
     for (const repo of repos) {
-      const { description, html_url, name } = repo;
+      const { description, html_url, name, homepage } = repo;
       list.innerHTML += `
       <li class="projects-list__item">
       <img class="projects-list__img" src="../assets/img/github.svg" alt="" />
-      <a class="projects-list__link" }">${name}</a>
+      <a class="projects-list__link" a >${name}</a>
       <p class="projects-list__link--description">${description ? description : "brak opisu"}</p>
       <div class=" projects-list__footer">
 
-        <button class="projects-list__footer--script">GITHUB</button>
-        <button class="projects-list__footer--demo">DEMO</button>
+        <a href="${html_url}" class="projects-list__footer--script projects-list__footer--script-js">GITHUB</a>
+        <a href="${homepage}" class="projects-list__footer--demo projects-list__footer--demo-js">DEMO</a>
       </div>
     </li>
      `;
-    }
+
+  }
+
   })
   .catch(err => {
     console.log(err);
   })
-/*demo.addEventListener('click' , (e) => {
+const list = document.querySelector('.projects__list--js');
+const script = document.querySelector(".projects-list__footer--script-js")
+const demo = document.querySelector(".projects-list__footer--demo-js")
+const skillsTitle = document.querySelector(".skills_container__title--js")
+ console.log(demo);
+console.log(script);
 
-}
-)*/
+
+
+
+
